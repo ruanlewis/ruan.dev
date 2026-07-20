6f0b247a7d7d3c3198f3154734c889bdd6628709
ruanlewis <ruanlewis70@gmail.com>
2026-07-20 11:23:39 +0200

    Server fix #10

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
 // @ts-ignore - importing the pre-bundled CJS output
-import app from "../dist/server.cjs";
+import * as serverModule from "../dist/server.cjs";
+
+const app: any = (serverModule as any).default ?? serverModule;
 
 export default function handler(req: Request, res: Response) {
   return app(req, res);
